d89855464854186b8a3a4823e82bdce4633e0fb1
Perry Kwarteng <perry.kwarteng@amalitech.com>
2024-05-14 08:21:39 +0000

    fix dark mode and country card

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { CountryCardComponent } from '../../shared/components/country-card/country-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CountryCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/shared/components/country-card/country-card.component.ts
+++ src/app/shared/components/country-card/country-card.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-country-card',
   standalone: true,
   imports: [],
   templateUrl: './country-card.component.html',
-  styleUrl: './country-card.component.css'
+  styleUrl: './country-card.component.css',
 })
 export class CountryCardComponent {
-
+  @Input() isDarkMode: boolean = false;
 }
